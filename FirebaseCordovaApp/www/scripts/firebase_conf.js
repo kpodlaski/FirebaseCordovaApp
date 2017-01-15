95e22d5d422512ce9c782cbd5f004c251a1a03a9
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2017-01-15 21:42:48 +0100

    Firebase Cordova app

---
+++ FirebaseCordovaApp/www/scripts/firebase_conf.js
@@ -0,0 +1,8 @@
+﻿var config = {
+    apiKey: "AIzaSyAztDgOfKn_PbbQUrYX1PvDv5ow2q565_U",
+    authDomain: "chatexample-b6dc3.firebaseapp.com",
+    databaseURL: "https://chatexample-b6dc3.firebaseio.com",
+    storageBucket: "chatexample-b6dc3.appspot.com",
+    messagingSenderId: "312753758783"
+};
+firebase.initializeApp(config);
